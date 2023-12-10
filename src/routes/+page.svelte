195a896cd413e9f69239e6369acb2591299629e9
Adam Soltys <asoltys@gmail.com>
2023-12-09 18:44:21 -0800

    create and scan routes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,11 @@
-<h1 class="text-2xl">Bitcoin Paper Wallet</h1>
-
 <div class="space-y-5">
-<button class="p-4 bg-black text-white rounded-2xl">Create new wallet</button>
-<button class="p-4 bg-black text-white rounded-2xl">Scan a QR code</button>
+	<h1 class="text-2xl">Bitcoin Paper Wallet</h1>
+	<a href="/create" class="flex gap-2 w-full md:w-60 p-4 bg-white border rounded-2xl justify-center">
+		<img src="/plus.svg" class="w-8" />
+    <div class="my-auto">Create new wallet</div>
+	</a>
+  <a href="/scan" class="flex gap-2 w-full md:w-60 p-4 bg-white border rounded-2xl justify-center">
+		<img src="/camera.svg" class="w-8" />
+    <div class="my-auto">Scan a QR code</div>
+  </a>
 </div>
