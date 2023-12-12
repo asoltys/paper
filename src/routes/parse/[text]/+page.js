491536a22f057adf21c02a09be7a429193c93c59
Adam Soltys <asoltys@gmail.com>
2023-12-12 10:06:14 -0800

    wif decode key

--- src/routes/parse/[text]/+page.js
+++ src/routes/parse/[text]/+page.js
@@ -1,10 +1,13 @@
 import { validate } from 'bitcoin-address-validation';
 import { redirect } from '@sveltejs/kit';
+import wif from 'wif';
 
 export async function load({ params }) {
 	let { text } = params;
 
 	if (validate(text)) throw redirect(307, `/address/${text}`);
 	if (text.startsWith('6')) throw redirect(307, `/decrypt/${text}`);
+	if (wif.decode(text)) throw redirect(307, `/key/${text}`);
+
 	return { text };
 }
